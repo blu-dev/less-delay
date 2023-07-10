d668a2e3ed2e86812a2d6f0775186bc496799b75
blu-dev <the.blu.dev@gmail.com>
2023-07-10 12:50:04 -0700

    fix crash

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,39 @@ unsafe fn run_scene_update(_: &skyline::hooks::InlineCtx) {
     }
 }
 
+std::arch::global_asm!(
+    r#"
+    .section .nro_header
+    .global __nro_header_start
+    .word 0
+    .word _mod_header
+    .word 0
+    .word 0
+    
+    .section .rodata.module_name
+        .word 0
+        .word 3
+        .ascii "hdr"
+    .section .rodata.mod0
+    .global _mod_header
+    _mod_header:
+        .ascii "MOD0"
+        .word __dynamic_start - _mod_header
+        .word __bss_start - _mod_header
+        .word __bss_end - _mod_header
+        .word __eh_frame_hdr_start - _mod_header
+        .word __eh_frame_hdr_end - _mod_header
+        .word __nx_module_runtime - _mod_header // runtime-generated module object offset
+    .global IS_NRO
+    IS_NRO:
+        .word 1
+    
+    .section .bss.module_runtime
+    __nx_module_runtime:
+    .space 0xD0
+    "#
+);
+
 #[no_mangle]
 pub extern "C" fn main() {
     unsafe {
